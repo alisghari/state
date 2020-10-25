16efafa79b09ffc4bcaed1db81db1c39fd96ae9b
alisghari <alisghari66@gmail.com>
2020-10-25 14:41:10 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -4,22 +4,30 @@ import './App.css';
 import image from './image.jpg'
 
 class App extends Component {
- 
-  constructor(){
-    super()
-    this.state= {
-      fullName:"Ali sghari ",bio: "time is money",profession: "Futur full-stack devoloper " ,imgSrc: image ,date: new Date(),show: false
-    };
-  }
-componentDidMount() {
+    
+    state={
+      fullName:"Ali sghari ",
+      bio: "time is money",
+      profession: "Futur full-stack devoloper",
+      imgSrc: image ,
+      time:0,
+      show: false,
+  };
 
+
+componentDidMount(){
+  setInterval(()=>{
+    let time = new Date()
+    time = time.getHours()+':'+time.getMinutes()
+    this.setState({time:time})
+  },1000)
 }
-componentUnMount() {
- 
-}
+  
+
+
 hide(){
   this.setState({
-    show: true
+    show: !this.state.show
   })
 }
    render() {
@@ -27,23 +35,25 @@ hide(){
      <>
 
      {
-        this.state.show?
+        this.state.show &&
         <div>
      <h3>fullName :  {this.state.fullName}</h3>
       <h3>bio  : {this.state.bio}</h3>
       <h3>profession  : {this.state.profession}</h3>
-     <img src={this.state.imgSrc}/>
+         <img src={this.state.imgSrc}/>
    
         <h1>Bonjour</h1>
-        <h2>Il est {this.state.date.toLocaleTimeString()}.</h2>
+        <h2>Il est {this.state.time}.</h2>
         </div>
-        :null
+        
      }
         <button onClick={()=>this.hide()}> show</button>
       
      </>
      );
+     
     }
   }
+  
 
 export default App ;
